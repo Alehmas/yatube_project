b4556d7235a046808383ff61e60d4771efad5c90
Олег Маслов <msOleg-2006@rambler.ru>
2021-11-18 21:45:21 +0300

    исправления в url

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.index),
-    path('posts/<slug:pk>/', views.group_posts),
+    path('', views.index),
+    path('groups/<slug:pk>/', views.group_posts),
 ]
