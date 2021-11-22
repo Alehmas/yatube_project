69c3c88d74646e5df17701c68f0711c223b71b86
Олег Маслов <msOleg-2006@rambler.ru>
2021-11-22 15:45:35 +0300

    Теперь текст title передается в шаблон из view-функций через словарь context

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,11 +4,24 @@ from django.shortcuts import HttpResponse, render
 # Функция обработки главной страницы
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
-
+    title = "Это главная страница проекта Yatube"
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
 
 # Функция обработки групп
 def group_posts(request, pk):
-    return HttpResponse(f'Пост группы {pk}')
+    template = 'posts/group_list.html'
+    title = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
 
+""" Функция обработки групп
+была так оформлена функция до раздела HTML страницы в Django. Render
+def group_posts(request, pk):
+    return HttpResponse(f'Пост группы {pk}')
+"""
 # Create your views here.
