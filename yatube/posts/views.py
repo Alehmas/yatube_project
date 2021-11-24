ee7bac14a7292e48bb5fa0733cfe624b342275ab
Олег Маслов <msOleg-2006@rambler.ru>
2021-11-24 21:55:46 +0300

    Создание модели Group

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import HttpResponse, render
 
+from .models import Post, Group
+
 
 # Функция обработки главной страницы
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+# Функция обработки групп
+def group_posts(request, pk):
+    group = get_object_or_404(Group, pk=pk)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+""" Функция обработки главной страницы
 def index(request):
     template = 'posts/index.html'
     title = "Это главная страница проекта Yatube"
@@ -10,7 +30,7 @@ def index(request):
     }
     return render(request, template, context)
 
-# Функция обработки групп
+Функция обработки групп
 def group_posts(request, pk):
     template = 'posts/group_list.html'
     title = "Здесь будет информация о группах проекта Yatube"
@@ -19,10 +39,5 @@ def group_posts(request, pk):
     }
     print(pk)
     return render(request, template, context)
-
-""" Функция обработки групп
-была так оформлена функция до раздела HTML страницы в Django. Render
-def group_posts(request, pk):
-    return HttpResponse(f'Пост группы {pk}')
 """
 # Create your views here.
