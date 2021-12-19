4d37dcda31f88172461a9849faf50660dad56404
Oleg Maslov <msOleg-2006@yandex.ru>
2021-12-19 18:54:13 +0300

    Добавление приложений users,about,core, шаблонов пользователя, навигации

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,18 +1,18 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self):
         return self.title
-    
-    
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -23,8 +23,14 @@ class Post(models.Model):
     )
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
         related_name='posts'
     )
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ['-pub_date']
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,36 +1,30 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render
 
-from .models import Post, Group
+from .models import Group, Post, User
 
 
-# Функция обработки главной страницы
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
-    posts = Post.objects.order_by('-pub_date')[:10]
+    """Функция обработки главной страницы"""
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     template = 'posts/index.html'
     title = 'Последние обновления на сайте'
-    # В словаре context отправляем информацию в шаблон
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
         'title': title,
     }
     return render(request, template, context)
 
-# Функция обработки групп
+
 def group_posts(request, slug):
-    # Функция get_object_or_404 получает по заданным критериям объект 
-    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
-    # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
+    """Функция обработки главной страницы"""
     group = get_object_or_404(Group, slug=slug)
     template = 'posts/group_list.html'
     title = f'Записи сообщества {group}'
-    # Метод .filter позволяет ограничить поиск по критериям.
-    # Это аналог добавления
-    # условия WHERE group_id = {group_id}
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()[:10]
     context = {
         'group': group,
         'posts': posts,
@@ -39,4 +33,33 @@ def group_posts(request, slug):
     return render(request, template, context)
 
 
-# Create your views here.
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    template = 'posts/profile.html'
+    title = f'Профайл пользователя {user.get_full_name()}'
+    post_list = Post.objects.filter(author=user).order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    posts_count = paginator.count
+    context = {
+        'page_obj': page_obj,
+        'title': title,
+        'posts_count': posts_count,
+        'user': user,
+    }
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+    template = 'posts/post_detail.html'
+    post = get_object_or_404(Post, pk=post_id)
+    author = post.author
+    posts_count = Post.objects.filter(author=author).count()
+    title = f'Пост {post.text[:30]}'
+    context = {
+        'post': post,
+        'posts_count': posts_count,
+        'title': title
+    }
+    return render(request, template, context)
