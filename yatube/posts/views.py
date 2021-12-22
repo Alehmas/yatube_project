26191af618a2a6d33fbaa7e1c51a8a699419025f
Олег Маслов <msOleg-2006@rambler.ru>
2021-12-22 11:17:38 +0300

    Добавлена возвможность создавать и редактировать посты

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,9 @@
 from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
 
 from .models import Group, Post, User
+from .forms import PostForm
 
 
 def index(request):
@@ -22,12 +24,15 @@ def index(request):
 def group_posts(request, slug):
     """Функция обработки главной страницы"""
     group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     template = 'posts/group_list.html'
     title = f'Записи сообщества {group}'
-    posts = group.posts.all()[:10]
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
         'title': title,
     }
     return render(request, template, context)
@@ -63,3 +68,34 @@ def post_detail(request, post_id):
         'title': title
     }
     return render(request, template, context)
+
+
+@login_required
+def post_create(request):
+    template = 'posts/create_post.html'
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            author = request.user
+            group = form.cleaned_data['group']
+            post = Post.objects.create(author=author, text=text, group=group)
+            post.save()
+            return redirect('posts:profile', author)
+        return render(request, template, {'form': form})
+    form = PostForm()
+    return render(request, template, {'form': form})
+
+
+@login_required
+def post_edit(request, post_id):
+    template = 'posts/update_post.html'
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST or None, instance=post)
+        if form.is_valid():
+            post.save()
+            return redirect('posts:post_detail', post_id)
+        return render(request, template, {'form': form})
+    form = PostForm(request.POST or None, instance=post)
+    return render(request, template, {'form': form})
