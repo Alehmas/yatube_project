31f3fdd5258a73b20a9e0d197c3f70ab29b7ba28
Олег Маслов <msOleg-2006@rambler.ru>
2022-01-26 11:35:39 +0300

    Исправления pep8

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.views import (LoginView, LogoutView,
+                                       PasswordChangeDoneView,
+                                       PasswordChangeView,
+                                       PasswordResetCompleteView,
+                                       PasswordResetConfirmView,
+                                       PasswordResetDoneView,
+                                       PasswordResetView)
 from django.urls import path
-from django.contrib.auth.views import (
-    LoginView, LogoutView, PasswordChangeDoneView, PasswordChangeView,
-    PasswordResetCompleteView, PasswordResetConfirmView,
-    PasswordResetDoneView, PasswordResetView
-)
 
 from . import views
 
